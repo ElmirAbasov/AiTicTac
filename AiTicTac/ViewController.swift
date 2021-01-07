711a6e716dc23f277f1a49499bf51b3b1af2c53e
Xizafren <m-ett@hotmail.se>
2021-01-07 14:33:30 +0100

    Initial Commit

---
+++ AiTicTac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AiTicTac
+//
+//  Created by Elmir Abasov on 2021-01-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
